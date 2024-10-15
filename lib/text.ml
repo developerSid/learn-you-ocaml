51b2174df1e26526f12a5394f7b3c183eb7aa139
developerSid <gary.w.myers@outlook.com>
2024-10-15 18:07:09 +0000

    Install utop into the devcontainer

--- lib/text.ml
+++ lib/text.ml
@@ -3,17 +3,20 @@ open Result
 module Text : sig
    type text
    type text2 = private Text2 of text
+   type trimmedText2 = private TrimmedText2 of text2
 
    val create_text: string -> text
    val create_text2: string -> (string -> 'error) -> (text2, 'error) result
 end = struct
    type text = string
    type text2 = Text2 of text
+   type trimmedText2 =  TrimmedText2 of text2 
 
    let create_text s = s
-   let create_text2 s (too_long_error_fn: string -> 'error) : (text2, 'error) result =
+   let create_text2 s too_long_error_fn =
       if String.length s <= 2 then
          Ok (Text2 s)
       else
          Error (too_long_error_fn s)
 end
+
